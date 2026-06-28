aaa8e332f7e94a44a83d06f6676e4e6b5917c4bf
otera <bad.lake3937@fastmail.com>
2026-06-28 17:52:16 +0900

    fix: 非推奨となったexternalizeDepsPluginの利用を停止

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin({ exclude: ['electron-store'] })]
+    build: {
+      externalizeDeps: {
+        exclude: ['electron-store']
+      }
+    }
   },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
+  preload: {},
   renderer: {
     plugins: [react()]
   }
